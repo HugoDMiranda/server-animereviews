717b5dc2e52a474ebe7c19b6044e59e480b27674
HugoDMiranda <hdmirandab@hotmail.com>
2023-06-16 16:05:29 -0500

    add createPool

--- db.js
+++ db.js
@@ -1,4 +1,4 @@
-import mysql from "mysql";
+import { createPool } from "mysql";
 import {
   DB_HOST,
   DB_DATABASE,
@@ -7,7 +7,7 @@ import {
   DB_PORT,
 } from "./config.js";
 
-export const db = mysql.createConnection({
+export const db = createPool({
   host: DB_HOST,
   user: DB_USER,
   password: DB_PASSWORD,
