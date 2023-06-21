b9a3fa43732560a06c67b7dd299de2ada546c72d
HugoDMiranda <hdmirandab@hotmail.com>
2023-06-21 14:20:39 -0500

    update cors-origin

--- index.js
+++ index.js
@@ -11,7 +11,26 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 
-app.use(cors());
+// app.use(
+//   cors({
+//     origin: ["http://localhost:3000"],
+//     methods: ["GET", "POST", "DELETE", "PUT"],
+//     credentials: true,
+//   })
+// );
+
+const origin = [
+  "https://anime-reviews-mauve.vercel.app/",
+  "http://localhost:3000",
+];
+
+app.use(
+  cors({
+    origin: origin,
+    methods: ["GET", "POST", "DELETE", "PUT"],
+    credentials: true,
+  })
+);
 
 app.use("/api/auth", authRoutes);
 app.use("/api/comments", commentsRoutes);
