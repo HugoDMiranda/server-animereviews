8bdbe85a2bee7bb22c78df8fa825b80e8542b549
HugoDMiranda <hdmirandab@hotmail.com>
2023-07-10 12:53:11 -0500

    planetscale DB update

--- config.js
+++ config.js
@@ -5,9 +5,9 @@ config();
 export const PORT = process.env.PORT || 3001;
 
 export const DB_HOST = process.env.DB_HOST || "aws.connect.psdb.cloud";
-export const DB_USER = process.env.DB_USER || "zqmyez4gyydobfu3u16y";
+export const DB_USER = process.env.DB_USER || "wwtkqtroumamtsr2gf4k";
 export const DB_PASSWORD =
   process.env.DB_PASSWORD ||
-  "pscale_pw_hsQQ4QOnCFUyFkrzj2pWbFNnmJMf10nqxQvHsiL9Aa3";
+  "pscale_pw_yIywIceSofmbyAtvO1xtftmoCC0nDPgJ8ybqjiVVKA6";
 export const DB_DATABASE = process.env.DB_DATABASE || "animereviews";
 export const DB_PORT = process.env.DB_PORT || 3306;
