70f88e6229ccbb7f3db4df81d719c2faaab2bd4d
HugoDMiranda <hdmirandab@hotmail.com>
2023-07-10 14:25:41 -0500

    vercel deploy

--- config.js
+++ config.js
@@ -4,10 +4,8 @@ config();
 
 export const PORT = process.env.PORT || 3001;
 
-export const DB_HOST = process.env.DB_HOST || "aws.connect.psdb.cloud";
-export const DB_USER = process.env.DB_USER || "wwtkqtroumamtsr2gf4k";
-export const DB_PASSWORD =
-  process.env.DB_PASSWORD ||
-  "pscale_pw_yIywIceSofmbyAtvO1xtftmoCC0nDPgJ8ybqjiVVKA6";
-export const DB_DATABASE = process.env.DB_DATABASE || "animereviews";
+export const DB_HOST = process.env.DB_HOST || "localhost";
+export const DB_USER = process.env.DB_USER || "root";
+export const DB_PASSWORD = process.env.DB_PASSWORD || "13032605";
+export const DB_DATABASE = process.env.DB_DATABASE || "cruddb";
 export const DB_PORT = process.env.DB_PORT || 3306;
